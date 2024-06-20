ee224fecebf58b07d2a6f44579c5a58b0cd70038
prerana <preranag840@gmail.com>
2024-06-20 13:55:52 +0530

    new file

---
+++ variable.js
@@ -0,0 +1,26 @@
+var name;
+name="prerana";
+console.log(typeof name);
+name=120
+console.log(typeof name);
+var a=10;
+console.log(typeof a);
+var a = "prerana";
+console.log(typeof a);
+name=150;
+console.log(name,typeof name);
+
+var refName="yash";
+console.log(refName);
+
+var refName="prerana";
+console.log(refName);
+
+let contactNo=080646535954;
+console.log(contactNo);
+contactNo=3543235274;
+console.log(contactNo);
+
+const numbers=2234266;
+console.log(numbers);
+
